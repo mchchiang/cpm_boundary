6820e69515cd55a441f18edb214db6a689ceb45f
mchchiang <michael_chiang_ch@hotmail.com>
2016-07-12 00:32:41 +0100

    Updated correlation code

--- src/cpm_boundary/Correlation.java
+++ src/cpm_boundary/Correlation.java
@@ -9,11 +9,11 @@ public class Correlation {
 		int startRow = Integer.parseInt(args[2]);
 		String filename = args[3];
 		String outputFile = args[4];
-		
+
 		double [][] data = new double [y][x];
-		
+
 		BufferedReader reader = new BufferedReader(new FileReader(filename));
-		
+
 		int count = 0;
 		String line;
 		String [] array;
@@ -33,22 +33,36 @@ public class Correlation {
 		}
 		reader.close();
 		
+
 		PrintWriter writer = new PrintWriter(
 				new BufferedWriter(new FileWriter(outputFile)));
-		
+
 		double sum;
 		int tmax;
+		double [] result = new double [y-startRow];
+		double avgSq = 0.0;
+		for (int i = 0; i < y-startRow; i++){
+			avgSq += data[i][1];
+		}
+		avgSq /= (double) (y-startRow);
+		avgSq *= avgSq;
+		
 		for (int tau = 0; tau < y-startRow; tau++){
-			System.out.println(tau);
+			//System.out.println(tau);
 			sum = 0.0;
 			tmax = y - tau - startRow;
 			for (int t = 0; t < tmax; t++){
-				sum += data[tau][1] * data[tau+t][1];
+				sum += data[t][1] * data[tau+t][1];
 			}
 			sum /= (double) tmax;
-			writer.printf("%d %.5f\n", tau, sum);
+			result[tau] = sum - avgSq;
 		}
-		
+
+		double norm = result[0];
+		for (int tau = 0; tau < result.length; tau++){
+			writer.printf("%d %.5f %.5f\n", tau, result[tau], result[tau] / norm);	
+		}
+
 		writer.close();
 	}
 }
